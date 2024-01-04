63ff0a68149ebe9ff85795c77dbb047fbd6506c5
Jesse Muniz <jmuniz@cdcsoftware.com>
2024-01-04 07:31:24 -0500

    Window and keypresses printing within stdout

--- src/main.c
+++ src/main.c
@@ -1,43 +1,43 @@
-#include "../lib/raylib/include/raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
 
-int main( int argc, char *argv[])
+int main ( int argc, char **argv )
 {
+    printf("Ordine-Pico\n");
+    printf("%s\n", "Testing this way");
 
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-
-    InitWindow(screenWidth, screenHeight, "ordine-pico");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(DARKGRAY);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL initialization failed: %s\n", SDL_GetError());
+        return 1;
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window creation failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Main event loop
+    SDL_Event event;
+    int quit = 0;
+
+    while (!quit) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = 1;
+            } else if (event.type == SDL_KEYDOWN) {
+                printf("You pressed: %s\n", SDL_GetKeyName(event.key.keysym.sym));
+            }
+        }
+
+        // Your rendering and game logic can go here
+
+        SDL_Delay(16); // Add a small delay to control the frame rate
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
